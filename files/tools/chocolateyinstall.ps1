2ca4f8607ce5fa6454ffb9e13786b1fd07fa4e95
Josh65-2201 <26977075+Josh65-2201@users.noreply.github.com>
2025-11-13 00:56:21 +0000

    GitHub: initial upload

---
+++ files/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$WindowsVersion=[Environment]::OSVersion.Version
+
+if (($WindowsVersion.Major -ne '10') -and ($WindowsVersion.Build -lt '18362')) {
+  Throw 'This Package requires at least Windows 10 Build 18362 or newer.'
+}
+
+$AppInstaller = Get-ChildItem -Path $toolsDir -Recurse -Filter "*.appinstaller"
+
+if ($AppInstaller) {
+  Add-AppxPackage -AppInstallerFile $AppInstaller.FullName
+} else {
+  Throw 'Unable to locate appinstaller. Please notify the maintainer.'
+}
---
+++ files/update.ps1
@@ -0,0 +1,33 @@
+import-module au
+
+$releases = 'https://api.github.com/repos/files-community/Files/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x32:).*"     = "`${1} $($Latest.URL32)"
+            "(?i)(checksum32:).*" = "`${1} $($Latest.Checksum32)"
+        }
+
+    }
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_GetLatest {
+    $latest_release = Invoke-RestMethod -Uri $releases
+
+    @{
+        Version      = $latest_release.tag_name.trim('v')
+        URL32        = 'https://cdn.files.community/files/stable/Files.Package.appinstaller'
+        ReleaseNotes = $latest_release.html_url
+    }
+}
+
+update -ChecksumFor none

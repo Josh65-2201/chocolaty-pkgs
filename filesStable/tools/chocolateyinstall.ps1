b55513fcac115ccb933a892620844976a7fbac24
Josh65-2201 <26977075+Josh65-2201@users.noreply.github.com>
2026-05-10 17:24:25 +0100

    Files: Updated to 4.1.0, FilesPreview: Added 4.1.0

---
+++ filesStable/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$WindowsVersion=[Environment]::OSVersion.Version
+
+if (($WindowsVersion.Major -ne '10') -and ($WindowsVersion.Build -lt '19041')) {
+  Throw 'This Package requires at least Windows 10 Build 19041 or newer.'
+}
+
+$AppInstaller = Get-ChildItem -Path $toolsDir -Recurse -Filter "*.appinstaller"
+
+if ($AppInstaller) {
+  Add-AppxPackage -AppInstallerFile $AppInstaller.FullName
+} else {
+  Throw 'Unable to locate appinstaller. Please notify the maintainer.'
+}
